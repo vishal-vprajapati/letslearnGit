d7b7a382a1160290978ed8e8cbd6ce367e069d3c
vishal prajapati <vishal.vp1545@gmail.com>
2022-02-23 08:49:57 +0530

    initial commit

---
+++ Pract.java
@@ -0,0 +1,25 @@
+public class Pract
+{
+    public static void main(String args[])
+{
+    int i,j,k=4;
+    for(i=4;i>0;i--){
+     for(j=i;j<5;j++)
+  
+      { 
+         while(k!=0)
+         {
+             System.out.print(" ");
+             
+             
+
+     k--;
+         }
+        System.out.print("*");
+    
+    }
+    System.out.println();
+
+}
+}
+}
